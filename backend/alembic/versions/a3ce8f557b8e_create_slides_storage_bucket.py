24a1dc1f3e977f01a139f9a393a89ad62d661ee7
fauzu-dheen <fauzudheen2@gmail.com>
2026-06-30 17:23:33 +0530

    chore: add slides storage bucket migration

---
+++ backend/alembic/versions/a3ce8f557b8e_create_slides_storage_bucket.py
@@ -0,0 +1,36 @@
+"""create slides storage bucket
+
+Revision ID: a3ce8f557b8e
+Revises: 5b7274f2c8b4
+Create Date: 2026-06-30 17:22:48.991182
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision: str = "a3ce8f557b8e"
+down_revision: Union[str, Sequence[str], None] = "5b7274f2c8b4"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.execute(
+        """
+        insert into storage.buckets (id, name, public)
+        values ('slides', 'slides', false)
+        on conflict (id) do update
+        set
+            name = excluded.name,
+            public = excluded.public
+        """
+    )
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.execute("delete from storage.buckets where id = 'slides'")
